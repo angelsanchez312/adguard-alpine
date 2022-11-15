afd476e9ac88362ac946fefc9a803b77793fa9a8
angelsanchez312 <102834581+angelsanchez312@users.noreply.github.com>
2022-11-15 16:25:20 -0600

    Update adguardhome-alpine.sh

--- adguardhome-alpine.sh
+++ adguardhome-alpine.sh
@@ -5,31 +5,21 @@ echo "https://github.com/AdguardTeam/AdGuardHome/releases"
 echo "uname -m lists your architecture as "; uname -m
 read aghurl
 
-elevated=doas
+RND1=$$
 
-# extract in tmp folder
-mkdir -p /tmp/AGH/
-cd /tmp/AGH/
-wget -P /tmp/AGH/ $aghurl 
-tar -xf /tmp/AGH/*.tar.gz
+# Extract archive from tmp folder
+mkdir -p /tmp/AGH.$RND1/
+wget -P /tmp/AGH.$RND1/ $aghurl 
+tar -xf /tmp/AGH.$RND1/*.tar.gz
 
-#EXEC: Command to move AdGuardHome into the /opt directory
-$elevated mv /tmp/AGH/AdGuardHome /opt/
+# Move AdGuardHome into the /opt/ directory
+mv /tmp/AGH.$RND1/AdGuardHome /opt/
 
+# Move file in repo to /etc/init.d/AdGuardHome
+mv ./AdGuardHome /etc/init.d/AdGuardHome
 
-# have AdGuardHome in the /opt/ directory
-# TODO: Choose a different location, press enter to skip
-
-
-# Get a file wriiteen to /etc/init.d/AdGuardHome
-# Currently, getting a file from server VS creating the file directly from the script
-$elevated wget -P /tmp/AGH/ http://git.local/AdGuardHome
-
-## Step 4 - Enable the service by running doas rc-update add AdGuardHome
-#$elevated rc-update add AdGuardHome
-
-## Step 5 - Start the service by running doas rc-service AdGuardHome start
-#$elevated rc-update AdGuardHome start
+rc-update add AdGuardHome
+rc-update AdGuardHome start
 
 echo "Done!"
-echo "Configure AdGuard URL by accessing it here at http://127.0.0.1:3000 or via its IP address!"
+echo "Configure AdGuard URL by accessing it here at http://127.0.0.1:3000 or by its ip address!"
